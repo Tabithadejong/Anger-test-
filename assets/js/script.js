856fda23e67d2bce69a70b1678b62e281da0fabf
Tabithadejong <tabithadejong654@gmail.com>
2022-10-02 09:52:18 +0000

    Bug report, radio input is not required because of function connected to it which will launch by default regardless of input, need to write a new function that will check for answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,20 @@
 let questionIndex = 0; 
 showQuestions(); 
 
+let input = document.getElementsByTagName("input"); 
+
+input.setAttribute("required"); 
+
 function showQuestions() { 
   document.getElementById("begin").style.display = "none"; 
+  
   let i; 
   let questions = document.getElementsByClassName("questions-container"); 
   for (i=0; i< questions.length; i++ ) { 
     questions[i].style.display = "none"; 
   }
   questionIndex++; 
-  if (questionIndex > questions.length) {questionIndex = 1}
+  if (questionIndex > questions.length) {alert("no more questions"); }
   questions[questionIndex-1].style.display= "block"; 
   
 
