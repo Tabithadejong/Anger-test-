1fc4996e069d0796c7fdbbb12abc3d8d946c5fe0
Tabithadejong <tabithadejong654@gmail.com>
2022-10-07 09:47:30 +0000

    tried out a game (wack a mole) withing the anger personality test, but due to increasing difficulty reseting files to previous state

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,17 @@
-let questionIndex = 0; 
+/* let questionIndex = 0; 
 showQuestions(); 
  
+/* write function for commence button  
+
+
+function letsBegin() {
+  document.getElementById("begin").style.display = "none"; 
+  document.getElementById("q1").style.display = "block"; 
+}
+
 
 function showQuestions() { 
-  document.getElementById("begin").style.display = "none"; 
+   
   
   let i; 
   let questions = document.getElementsByClassName("questions-container"); 
@@ -11,16 +19,70 @@ function showQuestions() {
     questions[i].style.display = "none"; 
   }
   questionIndex++; 
-  if (questionIndex > questions.length) {alert("no more questions"); }
   questions[questionIndex-1].style.display= "block"; 
+  if (questionIndex == questions.length) {alert("lets see the results"); }
+
+  let choice = document.querySelectorAll("input[type='radio']").value;
+  if (choice == "3") { alert("well done");} else if (choice == 2) { alert("even better"); } else if (choice == 1 ){alert("try again"); }
+  else { 
+    alert("please select"); 
+    return; 
+  }
+ 
+
 
   
 }
 
-let score = 0; 
+function countScore() { 
+  let score; []
 
-function checkScore() { 
-  let input = getElementsByClassName("input").innerText; 
+  let radio = document.querySelectorAll("input[type='radio']".checked); 
+  let choice = document.querySelectorAll("input[type='radio']").value;
+
+  if (radio == true ) { 
+    alert(`${ choice}`); 
+  }
   
-  
+}  */ 
+
+/* let questionIndex = document.getElementsByClassName("question-area").style.display = "block";
+
+
+function showQuestions (){ 
+   
+} */ 
+
+const square = document.querySelectorAll(".hole"); 
+const fire = document.querySelectorAll(".fire"); 
+const timeLeft= document.getElementById("time-left"); 
+let score = document.getElementById("score"); 
+
+let result= 0 ; 
+
+function randomSquare(){ 
+  square.forEach(className => { 
+    className.classList.remove("fire"); 
+  })
+  let randomPosition = square[Math.floor(Math.random()* 9 )]; 
+  randomPosition.classList.add("fire")
+
+  /* assign id of the randomPosition to hitPostition to use later */ 
+  hitPosition = randomPosition.id
 }
+
+square.forEach(id => { 
+  id.addEventListener("mouseup", () => { 
+    if(id.id === hitPosition) { 
+      result = result + 1
+      score.textContent = result
+    }
+  })
+})
+
+function moveFire() { 
+  let timerId = null ;
+  timerId = setInterval(randomSquare, 1000)
+}
+
+moveFire()
